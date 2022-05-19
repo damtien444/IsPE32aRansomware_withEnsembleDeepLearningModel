4a66bef056187adb68686728429d063dab7292a6
damti <damtien444@gmail.com>
2022-05-19 15:13:07 +0700

    update requirements.txt, rewrite content at landing page

--- predict_page.py
+++ predict_page.py
@@ -6,36 +6,54 @@ from predict import predict_single_file
 from utils.preprocessing import createRGBImageWithSectionAndPEBest
 
 tempfolder = r"E:\Malware Image Based\drive-download-20220515T075039Z-001\temp"
+weight_imcec_dir= r"result/exp/best.pt"
 
 def save_uploadedfile(uploadedfile, tempfolder):
     with open(os.path.join(tempfolder, uploadedfile.name), "wb") as f:
         f.write(uploadedfile.getbuffer())
     return os.path.join(tempfolder, uploadedfile.name)
 
-weight= r"result/exp/best.pt"
+
 def show_predict_page():
     st.title("Is this executable a Ransomware?")
 
-    st.write("### We need an Portable Executable 32bit file as the input of the prediction pipeline! ###")
+    st.write("To figure out the fact that a pe file is a ransomware is a pain process. In which the expert "
+             "have to do static and dynamic analysis. This tool is meant to help them to classify them easily.")
+
+    st.write("This tools use a complex ensemble deeplearning model (Resnet50 and VGG16 base model) trained on a data set "
+             "of 3000 samples including ransomware and other file.")
+
+    st.write("""The main contributions to this study include:\n
+1.   Classification of ransomware uses images containing information from PE headers, thereby helping to increase the similarity between samples of the same variant strain.\n
+2.   Select features from PE headers using machine learning models and encode them into images representing ransomware samples.\n
+3.   Building a combination model from famous CNN networks such as ResNet-50, VGG16. Using the new VisionTransformer model in the problem of ransomware classification.""")
+
+    st.markdown("The full report could be found " + f'<a href="data:file/pdf" download="utils/peFeaturesAdd_3 - ENGLISH.pdf">here.</a>', unsafe_allow_html=True)
+
+    st.write("Authors of the work: Dam Quang Tien, Nguyen Nghia Thinh, Le Viet Trung")
+
+    st.write("Faculty: Dr. Le Tran Duc")
+
+    st.write("## We need an Portable Executable 32bit file as the input of the prediction pipeline! ##")
 
     file = st.file_uploader(label="Upload file")
 
-    ok = st.button("Predict!")
+    ok = st.button("Let's predict!")
 
     if ok:
         tempfile = save_uploadedfile(file, tempfolder)
         try:
             saved = createRGBImageWithSectionAndPEBest(tempfile, withPe=True)
             image = Image.open(saved)
-            st.image(image)
-            pro, pre = predict_single_file(weight_dir=weight, image_path=saved, cuda=True)
+            st.image(image, caption=f"File's representative image with section markers and 5 color encoded PE features.", width=400)
+            pro, pre = predict_single_file(weight_dir=weight_imcec_dir, image_path=saved, cuda=False)
 
             predict = "not ransomware"
 
             if pre == 1:
                 predict = "ransomware"
 
-            st.write(f"### The file is {predict} with probability {pro * 100}% ###")
+            st.write(f"The file is {predict} with probability {pro * 100}%")
         except:
             st.write("The input file is not PE32 or corrupted!")
 
